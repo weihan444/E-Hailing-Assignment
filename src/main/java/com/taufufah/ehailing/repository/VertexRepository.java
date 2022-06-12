1d03eaf6dc575fffa420eefa8c827379cea5bb90
Wei Han Yau <weihanyau@gmail.com>
2022-06-13 00:29:57 +0800

    add CORS, add connectVertex, add small map for testing

--- src/main/java/com/taufufah/ehailing/repository/VertexRepository.java
+++ src/main/java/com/taufufah/ehailing/repository/VertexRepository.java
@@ -10,4 +10,7 @@ public interface VertexRepository extends Neo4jRepository<Vertex, Long> {
             "WITH v, point.distance(point({x:$x, y:$y}), point({x:v.longitude, y:v.latitude})) as distance " +
             "ORDER BY distance LIMIT 1 RETURN v")
     Vertex findClosestNodes(Double x, Double y);
+
+    @Query("MATCH (v1:Vertex) MATCH (v2:Vertex) WHERE v1.longitude = $x1 AND v1.latitude = $y1 AND v2.longitude = $x2 AND v2.latitude = $y2 CREATE (v1)-[:CONNECTED_VERTEX {distance: point.distance(point({x:v1.longitude, y:v1.latitude}), point({x:v2.longitude, y:v2.latitude}))}]->(v2) RETURN v2")
+    Vertex connectVertex(double x1, double y1, double x2, double y2);
 }
